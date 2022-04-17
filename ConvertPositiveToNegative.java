4c3412d8928b17fc8fb9034fce0276f0b525656f
HeenaNousheen <103849761+HeenaNousheen@users.noreply.github.com>
2022-04-17 21:51:58 +0530

    Add files via upload

---
+++ ConvertPositiveToNegative.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class ConvertPositiveToNegative {
+	public static void main(String[] args) {
+		int number = -40;
+		int convert;
+		if(number<0)
+		{
+			convert = number*(-1);
+					System.out.println("The number " + number + "is converted to " + convert);
+		}
+	}
+
+}
